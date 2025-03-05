76da9febbb63069b8827db566242444a6387dcb5
Hung Nguyen <phuchung.nguyen78@gmail.com>
2025-03-05 14:45:02 +0700

    new file

--- force-app/main/default/lwc/customDataTablePicklist/customDataTablePicklist.js
+++ force-app/main/default/lwc/customDataTablePicklist/customDataTablePicklist.js
@@ -1,14 +1,26 @@
 import LightningDatatable from 'lightning/datatable';
+import minimumRatingColumn from './minimumRatingColumn.html';
 import picklistColumn from './picklistColumn.html';
-import pickliststatic from './pickliststatic.html'
+import primarySkillColumn from './primarySkillColumn.html';
+// import pickliststatic from './pickliststatic.html';
  
 export default class CustomDataTablePicklist extends LightningDatatable {
     static customTypes = {
         picklistColumn: {
-            template: pickliststatic,
-            editTemplate: picklistColumn,
+            template: picklistColumn,
+            // editTemplate: picklistColumn,
             standardCellLayout: true,
-            typeAttributes: ['label', 'placeholder', 'options', 'value', 'context', 'variant','name']
-        }
+            typeAttributes: ['label', 'placeholder', 'options', 'value', 'context', 'variant','name', 'handleChange']
+        },
+        primarySkillColumn: {
+            template: primarySkillColumn,
+            standardCellLayout: true,
+            typeAttributes: ['label', 'placeholder', 'options', 'value', 'context', 'variant','name', 'handleChange', 'rowId']
+        },
+        minimumRatingColumn: {
+            template: minimumRatingColumn,
+            standardCellLayout: true,
+            typeAttributes: ['label', 'placeholder', 'options', 'value', 'context', 'variant','name', 'handleChange', 'rowId']
+        },
     };
 }
